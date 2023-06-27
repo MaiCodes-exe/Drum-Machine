18005e4626bdd8e98ea9094d5746d6c14efb0510
MaiCodes-exe <maise.b.costa@gmail.com>
2023-06-27 21:10:52 +0100

    removing template code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,15 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
- 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
     </div>
   );
 }
