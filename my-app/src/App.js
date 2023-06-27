db93e4117250c7c7010e6990052abe7c2cd469c6
MaiCodes-exe <maise.b.costa@gmail.com>
2023-06-27 21:09:46 +0100

    removing template code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+ 
         <a
           className="App-link"
           href="https://reactjs.org"
