daaee9296811aed685b5f6165b13cdf10bcad4c4
MaiCodes-exe <maise.b.costa@gmail.com>
2023-06-27 21:08:18 +0100

    removing template code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
