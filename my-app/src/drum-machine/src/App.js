033056074bd373d9f957a8c0839dd568928b2806
MaiCodes-exe <maise.b.costa@gmail.com>
2023-07-04 21:19:53 +0100

    trying a new approcah

--- my-app/src/drum-machine/src/App.js
+++ my-app/src/drum-machine/src/App.js
@@ -1,10 +1,24 @@
+// App.js
 import React from 'react';
-import DrumMachine from './components/DrumMachine';
+import './App.css';
+import DrumPad from './components/DrumPad';
+
+const drumPads = [
+  { keyTrigger: 'Q', soundUrl: 'assets/sounds/kick.wav' },
+  { keyTrigger: 'W', soundUrl: 'assets/sounds/snare.wav' },
+  { keyTrigger: 'E', soundUrl: 'assets/sounds/hi-hat.wav' },
+  // Add more sounds here
+];
 
 const App = () => {
   return (
     <div className="App">
-      <DrumMachine />
+      <h1>Drum Machine</h1>
+      <div className="drum-pads">
+        {drumPads.map((pad, index) => (
+          <DrumPad key={index} keyTrigger={pad.keyTrigger} soundUrl={pad.soundUrl} />
+        ))}
+      </div>
     </div>
   );
 };
