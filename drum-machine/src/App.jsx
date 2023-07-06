0ec57d73fc34fd2e1b0befacdfbba6fa664134c5
MaiCodes-exe <maise.b.costa@gmail.com>
2023-07-06 21:50:17 +0100

    testing solution

---
+++ drum-machine/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DrumMachine from './DrumMachine';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My App</h1>
+      <DrumMachine />
+    </div>
+  );
+};
+
+export default App;
