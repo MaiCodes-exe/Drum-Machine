29c8c7102d23c74e6bb63fa7507e92a5f8c60dc4
MaiCodes-exe <maise.b.costa@gmail.com>
2023-07-07 21:32:08 +0100

    testing link fix

--- drum-machine/src/DrumMachine.jsx
+++ drum-machine/src/DrumMachine.jsx
@@ -12,7 +12,7 @@ const DrumMachine = () => {
       <div>
         <button onClick={() => handlePlaySound('path-to-kick-sound.wav')}>Kick</button>
         <button onClick={() => handlePlaySound('path-to-snare-sound.wav')}>Snare</button>
-        <button onClick={() => handlePlaySound('hi-hat.wav')}>Hi-Hat</button>
+        <button onClick={() => handlePlaySound('./sounds/hi-hat.wav')}>Hi-Hat</button>
       </div>
     </div>
   );
