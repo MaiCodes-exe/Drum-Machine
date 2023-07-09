7fab7431406b24797aa4a126ebd7f6fe9469a148
MaiCodes-exe <maise.b.costa@gmail.com>
2023-07-09 21:58:50 +0100

    adding more classes for styling

--- drum-machine/src/DrumMachine.jsx
+++ drum-machine/src/DrumMachine.jsx
@@ -11,12 +11,12 @@ const DrumMachine = () => {
     <div>
       <h1>Drum Machine</h1>
       <div id="pad">
-        <button id="but" onClick={() => handlePlaySound('path-to-kick-sound.wav')}>Kick</button>
-        <button id="but"></button>
-        <button id="but" onClick={() => handlePlaySound('path-to-snare-sound.wav')}>Snare</button>
-        <button id="but"></button>
-        <button id="but" onClick={() => handlePlaySound('./sounds/hi-hat.wav')}>Hi-Hat</button>
-        <button id="but"></button>
+        <button id="but" className='one' onClick={() => handlePlaySound('path-to-kick-sound.wav')}>Kick</button>
+        <button id="but" className='two'></button>
+        <button id="but" className='three' onClick={() => handlePlaySound('path-to-snare-sound.wav')}>Snare</button>
+        <button id="but" className='four'></button>
+        <button id="but"  className='five'onClick={() => handlePlaySound('./sounds/hi-hat.wav')}>Hi-Hat</button>
+        <button id="but" className='six'></button>
       </div>
     </div>
   );
