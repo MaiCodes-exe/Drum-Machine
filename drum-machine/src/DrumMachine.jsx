708d4bf9e95dfe959f7f12f2afc8548ed3cfb361
MaiCodes-exe <maise.b.costa@gmail.com>
2023-07-09 21:34:27 +0100

    adding buttons

--- drum-machine/src/DrumMachine.jsx
+++ drum-machine/src/DrumMachine.jsx
@@ -12,8 +12,11 @@ const DrumMachine = () => {
       <h1>Drum Machine</h1>
       <div id="pad">
         <button id="but" onClick={() => handlePlaySound('path-to-kick-sound.wav')}>Kick</button>
+        <button id="but"></button>
         <button id="but" onClick={() => handlePlaySound('path-to-snare-sound.wav')}>Snare</button>
+        <button id="but"></button>
         <button id="but" onClick={() => handlePlaySound('./sounds/hi-hat.wav')}>Hi-Hat</button>
+        <button id="but"></button>
       </div>
     </div>
   );
